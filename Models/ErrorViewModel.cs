71102f78116f54af7ee7fe971b4aa4abcb2a57bb
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-11-22 21:27:01 -0300

    inserido controller e model

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace bootcamp_potencial_net_developer_dio_mvc.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+  public string? RequestId { get; set; } = String.Empty;
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
