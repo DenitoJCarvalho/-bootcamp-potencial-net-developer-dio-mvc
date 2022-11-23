e83a8c7c900bdbdf27712e4af56fddc9516bd945
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-11-22 21:27:37 -0300

    Inserido view

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bootcamp_potencial_net_developer_dio_mvc.Context;
+
+namespace bootcamp_potencial_net_developer_dio_mvc.Controllers
+{
+  public class ContatoController : Controller
+  {
+    private readonly bootcamp_potencial_net_developer_dio_mvc.Context.Context _context;
+
+    public ContatoController(
+        bootcamp_potencial_net_developer_dio_mvc.Context.Context context
+    )
+    {
+      this._context = context;
+    }
+
+    #region Index
+    public IActionResult Index()
+    {
+      var contatos = _context.Contatos.ToList();
+      return View(contatos);
+    }
+    #endregion
+
+    #region Criar
+    public IActionResult Criar()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Criar(bootcamp_potencial_net_developer_dio_mvc.Models.Contato contato)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Contatos.Add(contato);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+      }
+      else
+      {
+        return View(contato);
+      }
+    }
+
+    #endregion
+  }
+}
